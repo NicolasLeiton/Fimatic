30870a3935d7e44ef7d3ace6731f835cbb4954eb
NicolasLeiton <nicolas.leitonp@utadeo.edu.co>
2024-09-24 13:28:13 -0500

    Firts commit, full view beta 1

---
+++ app/src/main/java/com/utadeo/fimatic/Niveles.kt
@@ -0,0 +1,35 @@
+package com.utadeo.fimatic
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Niveles : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_niveles)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun ir_home(view: View){
+        val home= Intent(this, MainActivity::class.java)
+        startActivity(home)
+    }
+    fun ir_abajo(view: View){
+        val home= Intent(this, Niveles2::class.java)
+        startActivity(home)
+    }
+    fun ir_nivel1(view: View){
+        val home= Intent(this, Level1::class.java)
+        startActivity(home)
+    }
+}
